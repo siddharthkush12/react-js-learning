2c8f618d08a61e34082a678f03bac077e6413c01
siddharthkush12 <siddharth5044549@gmail.com>
2025-02-08 18:18:11 +0530

    project

---
+++ 05_bg-changer/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import React from 'react'
+
+
+function App() {
+  let [color,setColor]=useState("grey");
+
+
+  return (
+    <>
+      <div className='w-full h-screen bg-amber-200 duration-200'></div>
+    </>
+
+  )
+}
+
+export default App 
