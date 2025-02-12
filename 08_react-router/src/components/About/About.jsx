0de0de9269c182403fb4a64c4108c1e0b8bc468e
siddharthkush12 <siddharth5044549@gmail.com>
2025-02-12 22:43:52 +0530

    cd

--- 08_react-router/src/components/About/About.jsx
+++ 08_react-router/src/components/About/About.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 
 function About() {
   return (
-    <>
-    <div>About</div>
-    <div>About</div>
-    <div>About</div>
-    <div>About</div>
-    <div>About</div>
-    <div>About</div>
-    <div>About</div>
-    <div>About</div>
-    </>
+    <div className="max-w-screen-xl mx-auto p-5">
+  <div className="flex flex-col sm:flex-row justify-between items-center gap-10">
+    <img src="about.png" alt="" className="w-full max-w-sm h-auto" />
+    <div className="py-10 sm:py-60 px-5 sm:px-10 text-center sm:text-left">
+      <h1 className="font-bold text-3xl py-5">
+        React development is carried out by passionate developers
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi hic iste quod quo magni blanditiis. Iste quaerat sunt ullam voluptate, vitae numquam magnam quam! Minima fugiat quisquam praesentium, facilis blanditiis quae ab voluptate temporibus officiis animi accusantium id deserunt porro atque veniam quo, accusamus expedita ratione voluptatem labore velit.
+      </p>
+    </div>
+  </div>
+</div>
   )
 }
 
--- 08_react-router/src/components/Contact.jsx/Contact.jsx
+++ 08_react-router/src/components/Contact.jsx/Contact.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
-
+import React from "react";
+import { CiLocationOn } from "react-icons/ci";
+import { CiMail } from "react-icons/ci";
+import { FaPhoneAlt } from "react-icons/fa";
 function Contact() {
   return (
-    <>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    </>
-  )
+    <div className="max-w-screen-xl mx-auto">
+      <div className="p-40 py-auto flex jsutify-between">
+        <div className="bg-gray-200 rounded-2xl p-6">
+          <h2 className="text-3xl font-bold">Get in touch:</h2>
+          <p className="py-2">Fill the form to start the conversation</p>
+          <div className="py-9 space-y-3 text-gray-900">
+            <p className="flex items-center">
+              <CiLocationOn className="text-2xl mr-2" />
+              AcmeInc, Street, State, Postal Code
+            </p>
+            <p className="flex items-center">
+              <FaPhoneAlt className="text-2xl mr-2" />
+              +91 7380339254
+            </p>
+            <p className="flex items-center">
+              <CiMail className="text-2xl mr-2" />
+              Siddharthkush12@gmail.com
+            </p>
+          </div>
+        </div>
+        <div className="p-6 ml-10">
+          <form action="submit" className="flex flex-col gap-5">
+            <input type="text" placeholder="Full Name" className="border-1 border-gray-400 text-[15px] rounded-xl p-3  w-80" />
+            <input type="text" placeholder="Email" className="border-1 border-gray-400 text-[15px] rounded-xl p-3  w-80" />
+            <input type="text" placeholder="Telephone Number" className="border-1 border-gray-400 text-[15px] rounded-xl p-3  w-80" />
+            <button className="bg-yellow-500 rounded-2xl py-3 text-2xl">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
